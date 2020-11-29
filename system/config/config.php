ae8a29a2c7b7b28e1acc3d1356fb4f36e09adfc0
Fernando Celmer <email@fernandocelmer.com>
2020-11-29 02:26:09 -0300

    Auto settings loading

---
+++ system/config/config.php
@@ -0,0 +1,32 @@
+<?php
+
+class Config {
+
+    private $base;
+
+    public function __construct() {
+        $this->base = parse_ini_file('.env', true);
+        foreach ($this->base as $key => $value) {
+            $_ENV[$key] = $value;
+        }
+    }
+
+    function env_app($tag = NULL){
+        if($tag){
+            return $_ENV['app'][$tag];
+        }else{
+            return False;
+        }
+    }
+
+    function env_db($tag = NULL) {
+        if($tag){
+            return $_ENV['database'][$tag];
+        }else{
+            return False;
+        }
+    }
+
+}
+
+?>
