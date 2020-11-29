495157ac0c98ab072d1163ec85b664f394f04631
Fernando Celmer <email@fernandocelmer.com>
2020-11-29 16:39:42 -0300

    Changing the function that takes the environment parameters

--- system/config/config.php
+++ system/config/config.php
@@ -5,23 +5,15 @@ class Config {
     private $base;
 
     public function __construct() {
-        $this->base = parse_ini_file('.env', true);
+        $this->base = parse_ini_file('.env');
         foreach ($this->base as $key => $value) {
             $_ENV[$key] = $value;
         }
     }
 
-    function env_app($tag = NULL){
+    function env($tag = NULL){
         if($tag){
-            return $_ENV['app'][$tag];
-        }else{
-            return False;
-        }
-    }
-
-    function env_db($tag = NULL) {
-        if($tag){
-            return $_ENV['database'][$tag];
+            return $_ENV[$tag];
         }else{
             return False;
         }
@@ -29,6 +21,6 @@ class Config {
 
 }
 
-$app = new Config();
+new Config();
 
 ?>
