c15209946885fbfb5c61171442b024222cfc89ca
Fernando Celmer <email@fernandocelmer.com>
2020-12-06 00:08:18 -0300

    Functions

---
+++ system/config/Functions.php
@@ -0,0 +1,17 @@
+<?php
+
+
+/**
+ * This class defines the main standard methods that will be used in the system.
+ */
+class Config {
+
+    private $config;
+
+    public function __construct() {
+
+    }
+
+}
+
+?>
