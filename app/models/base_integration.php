82bb1d530372bff3d3b91414e644993d82a8965c
Fernando Celmer <email@fernandocelmer.com>
2020-12-07 00:05:29 -0300

    BaseIntegration Class Model Implementation

--- app/models/base_integration.php
+++ app/models/base_integration.php
@@ -1,6 +1,6 @@
 <?php
 
-include_once "../../system/config/DataBase.php";
+include_once "../../system/DataBase.php";
 
 /**
  * This class defines the main parameters in the Base Integration table.
@@ -13,7 +13,17 @@ class BaseIntegration extends DataBase {
 
     function __construct($data = null) {
         $this->structureTable();
-        $this->data_init = $this->initData($data, $this->data_log);
+
+        if($data){
+            $data = $this->initData($data, $this->data_log);
+
+            $this->data_columns = $this->initColumns($data, 0);
+            $this->data_columns_values = $this->initColumns($data, 1);
+            $this->data_init = $this->dataPreparation($data, $this->data_log);
+        }
+
+        $this->database = new DataBase();
+        $this->db = $this->database->initDatabase();
     }
 
     /**
@@ -33,7 +43,13 @@ class BaseIntegration extends DataBase {
     }
 
     function query_insert(){
-        return $this->data_init;
+        $query = $this->database->data_insert(
+            $this->table_name,
+            $this->data_columns,
+            $this->data_columns_values,
+            $this->data_init
+        );
+        return $query;
     }
 
     function query_update(){
